6d5f95837b0378484ca21beb98f8e401107fbd20
Remi Gilliot <remi.gilliot98@gmail.com>
2026-08-22 18:43:27 +0800

    ex01 let's go

---
+++ module-6/ex01/serialize.cpp
@@ -0,0 +1,47 @@
+#include <cstdint>
+#include <iostream>
+
+struct Data
+{
+	int x;
+	int y;
+};
+
+
+uintptr_t serialize(Data* ptr)
+{
+	uintptr_t int_ptr;
+	int_ptr = reinterpret_cast<uintptr_t>(ptr);
+	return int_ptr;
+}
+
+Data* deserialize(uintptr_t raw)
+{
+	Data* ptr;
+	ptr = reinterpret_cast<Data*>(raw);
+	return ptr;
+}
+
+int main()
+{
+	Data a;
+	a.x = 42;
+	a.y = 21;
+
+	std::cout << "Values before serializing: {" << a.x << ", " << a.y << "}\n";
+	std::cout << "address of data before serializing: " << &a << std::endl;
+	uintptr_t int_ptr;
+	int_ptr = serialize(&a);
+	std::cout << int_ptr << std::endl;
+	std::cout << "address of data after serialization: " << &a << std::endl;
+	std::cout << "Values after serializing: {" << a.x << ", " << a.y << "}\n";
+
+	Data* p = deserialize(int_ptr);
+	std::cout << "address of original value of data pointer: " << &a << std::endl;
+	std::cout << "address of data after deserialize:	   " << p << std::endl;
+	std::cout << "Values after deserializing: {" << a.x << ", " << a.y << "}\n";
+
+
+	
+	return 0;
+}
