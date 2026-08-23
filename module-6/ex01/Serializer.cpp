3dcb3fbb73cb915c15ec6881b2173ca5855f61fd
Remi Gilliot <remi.gilliot98@gmail.com>
2026-08-23 19:01:03 +0800

    push ex01

--- module-6/ex01/Serializer.cpp
+++ module-6/ex01/Serializer.cpp
@@ -1,21 +1,15 @@
 #include <cstdint>
 #include <iostream>
+#include "Serializer.hpp"
 
-struct Data
-{
-	int x;
-	int y;
-};
-
-
-uintptr_t serialize(Data* ptr)
+uintptr_t Serializer::serialize(Data* ptr)
 {
 	uintptr_t int_ptr;
 	int_ptr = reinterpret_cast<uintptr_t>(ptr);
 	return int_ptr;
 }
 
-Data* deserialize(uintptr_t raw)
+Serializer::Data* Serializer::deserialize(uintptr_t raw)
 {
 	Data* ptr;
 	ptr = reinterpret_cast<Data*>(raw);
@@ -24,24 +18,22 @@ Data* deserialize(uintptr_t raw)
 
 int main()
 {
-	Data a;
+	Serializer::Data a;
 	a.x = 42;
 	a.y = 21;
 
 	std::cout << "Values before serializing: {" << a.x << ", " << a.y << "}\n";
 	std::cout << "address of data before serializing: " << &a << std::endl;
 	uintptr_t int_ptr;
-	int_ptr = serialize(&a);
+	int_ptr = Serializer::serialize(&a);
 	std::cout << int_ptr << std::endl;
 	std::cout << "address of data after serialization: " << &a << std::endl;
 	std::cout << "Values after serializing: {" << a.x << ", " << a.y << "}\n";
 
-	Data* p = deserialize(int_ptr);
+	Serializer::Data* p = Serializer::deserialize(int_ptr);
 	std::cout << "address of original value of data pointer: " << &a << std::endl;
 	std::cout << "address of data after deserialize:	   " << p << std::endl;
 	std::cout << "Values after deserializing: {" << a.x << ", " << a.y << "}\n";
 
-
-	
 	return 0;
 }
