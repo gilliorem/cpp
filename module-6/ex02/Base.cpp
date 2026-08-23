c3a764a7ef053777e70d39f3a594b0be1c225b47
gilliorem <remi.gilliot98@gmail.com>
2026-08-23 22:44:58 +0800

    ex02

--- module-6/ex02/Base.cpp
+++ module-6/ex02/Base.cpp
@@ -1,7 +1,91 @@
 #include "Base.hpp"
+#include <ctime>
+#include <iostream>
+
+Base::~Base(){}
+
+Base* generate(void)
+{
+	Base* basePtr;
+
+	time_t sec = time(NULL);		
+	if (sec % 3 == 0)
+	{
+		basePtr = new A;
+	}
+	else if (sec % 4 == 0)
+	{
+		basePtr = new B;
+	}
+	else 
+	{
+		basePtr = new C;
+	}
+	return basePtr;
+}
+
+void identify(Base* ptr)
+{
+	Base* tmp;
+
+	tmp = dynamic_cast<A*>(ptr);
+	if (tmp)
+	{
+		std::cout << "A\n";
+	}
+	if (!tmp)
+	{
+		Base* newTmp = dynamic_cast<B*>(ptr);
+		if (newTmp)
+		{
+			std::cout << "B\n";
+		}
+		else
+		{
+			newTmp = dynamic_cast<C*>(ptr);
+			std::cout << "C\n";
+		}
+	}
+}
+
+void identify(Base& ptr)
+{
+	try
+	{
+		ptr = dynamic_cast<A&>(ptr);
+		std::cout << "A\n";
+	}
+	catch(...)
+	{
+	}
+	try
+	{
+		ptr = dynamic_cast<B&>(ptr);
+		std::cout << "B\n";
+	}
+	catch(...)
+	{
+	}
+	try
+	{
+		ptr = dynamic_cast<C&>(ptr);
+		std::cout << "C\n";
+	}
+	catch(...)
+	{
+	}
+}
+
 
 int main()
 {
+	Base* basePtr = generate();
+	identify(basePtr);
+
+	Base* secondBasePtr = generate();
+	identify(*secondBasePtr);
+	
+
 
 	return 0;
 }
--- module-6/ex02/Base.hpp
+++ module-6/ex02/Base.hpp
@@ -1,5 +1,25 @@
-
+#pragma once
 class Base
 {
-	
+	public:
+		virtual ~Base();
 };
+
+class A: public Base
+{
+};
+
+class B: public Base
+{
+};
+
+class C: public Base
+{
+};
+
+
+Base* generate(void);
+
+void identify(Base* basePtr);
+
+void identify(Base& baseRef);
